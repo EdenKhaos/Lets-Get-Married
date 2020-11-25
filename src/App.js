255fb75c3b8ec3431b5ef378e54046ed16c970e7
Heather <raven.dantes@ymail.com>
2020-11-24 21:34:33 -0700

    add other map search pages to repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import "./App.css";
 import VenuePage from "./pages/Venue";
+import CateringPage from "./pages/Catering";
+import LodgingPage from "./pages/Lodging";
+
 function App() {
   return <VenuePage />;
 }
+
+// function App() {
+//   return <CateringPage />;
+// }
+
+// function App() {
+//   return <LodgingPage />;
+// }
+
 export default App;
