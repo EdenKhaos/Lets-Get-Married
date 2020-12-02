fad41c289f7043bb82d2b02e82564c8e9b8c5ea3
hannahjorich <hannah.jorich@gmail.com>
2020-12-01 20:36:35 -0700

    added elements to welcome page

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import InfoCard from "../InfoCard";
+import Carousel from "../Carousel";
+import venue from './images/venue.jpg';
+import catering from './images/catering.jpg';
+import lodging from './images/lodging.jpg';
+
+const Main = () => {
+    return (
+          <div className="col-12">
+            <Carousel />
+          </div>
+    )
+}
+
+export default Main;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
+import Main from "../../components/Main/Main";
+import "/.style.css";
+
+const Home = () => {
+    return (
+        <>
+        <Nav />
+        <Main />
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
