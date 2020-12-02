0390842ff9105e4cd9c5eb43eb3386c3754f018b
hannahjorich <hannah.jorich@gmail.com>
2020-12-01 21:37:28 -0700

    testing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ function Navagation () {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#features">Guestlist</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
